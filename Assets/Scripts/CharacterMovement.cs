05bb42ebe2f372197c51093b94fe3687ec67ba6f
ErikrEe <56590091+ErikrEe@users.noreply.github.com>
2020-02-02 11:19:00 +0100

    Movement Animation

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -11,6 +11,8 @@ public class CharacterMovement : MonoBehaviour
 
     Vector2 movement;
 
+    public Animator anim;
+
 
     void Start()
     {
@@ -21,6 +23,10 @@ public class CharacterMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Vertical", movement.y);
+        anim.SetFloat("Speed", movement.sqrMagnitude); 
     }
 
 
